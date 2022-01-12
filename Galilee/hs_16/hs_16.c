e88c1253ad2cb4d7883cd26067549fe531a2fffd
JoergGalilee <ugalilee@yahoo.fr>
2022-01-12 20:41:56 +0100

    Mine

---
+++ Galilee/hs_16/hs_16.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#pragma pack(1)
+
+
+
+struct Geo
+{
+
+
+};
+struct messwert
+{
+	int iMessungNummer;
+  	double dLuftdruck;
+  	float fTemperatur[3];
+ 
+};
+
+void output(struct messwert);
+int main ()
+{
+	struct messwert messung={12,14,13,4};
+	printf("The Size of Messung %ld: ",sizeof(messung)); 
+	//printf("The Size of the first number  %ld: The size of second number %ld: ",sizeof(messung.firstnumber , messung.secondnumber)); 
+	printf("fTemperatur nach Veränderungr  %lf : ",messung.fTemperatur[3]); 
+	return 0;
+}
